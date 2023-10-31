195b5b282af39da83ed3e41ec2ec85e4bc3e101e
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2023-10-31 22:10:36 +0530

    url

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 // db connection
+const BASE_URL = process.env.BASE_URL;
+const admin_base = process.env.ADMIN_BASE
+const QUESTION_URL = process.env.QUESTION_URL;
 require('./config/dataBase').dbConnect();
 const admin = require('./routers/adminRouter');
 const user = require('./routers/userRouter');
-app.use('/anurag/v1',user);
-app.use('/admin',admin);
-app.use('/anurag/v1/questions',require('./routers/questionRouter'));
+app.use(BASE_URL,user);
+app.use(admin_base,admin);
+app.use(QUESTION_URL,require('./routers/questionRouter'));
 // app listing
 // port 
 const PORT = process.env.PORT || 7000;
